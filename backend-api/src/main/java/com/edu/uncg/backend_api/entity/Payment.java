4457da54f2d23e7a81520af736bca165470a14b6
Ayanna Garrett <asgarrett@uncg.edu>
2026-03-23 16:19:04 -0400

    error fix

--- backend-api/src/main/java/com/edu/uncg/backend_api/entity/Payment.java
+++ backend-api/src/main/java/com/edu/uncg/backend_api/entity/Payment.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Payment {
@@ -13,7 +13,7 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long paymentID;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "booking_id")
     private Booking booking;
 
